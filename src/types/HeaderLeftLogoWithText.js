b4f498560c205eb5ef3eea8f9d699e7200c118fa
Alfa Irawan <alfa.irawan@outlook.com>
2019-07-31 10:44:14 +0700

    v0.1.19

--- src/types/HeaderLeftLogoWithText.js
+++ src/types/HeaderLeftLogoWithText.js
@@ -13,7 +13,7 @@ class HeaderLeftLogoWithText extends Component {
             <header style={ Object.assign({ width: '100%', display: 'flex', flexWrap: 'nowrap', alignContent: 'flex-end', alignSelf: 'center', alignItems: 'center' }, { padding: '20px 0' }, this.props.containerStyle) }>
                 <div id='id-header-left-logo-with-text-left-image-container' className='header-left-logo-with-text-left-image-container' style={ Object.assign({ marginLeft: 30 }, this.props.imageContainerStyle) }>
                     <img id='id-header-left-logo-with-text-left-image' className='header-left-logo-with-text-left-image' style={ Object.assign({ height: 50 }, this.props.imageStyle) } src={ this.props.logo } />
-                </div>b
+                </div>
                 <div id='id-header-left-logo-with-text-left-text-container' className='header-left-logo-with-text-left-text-container' style={ Object.assign({ marginLeft: 10, paddingLeft: 10 }, this.props.textContainerStyle) } >
                     <div className='header-left-logo-with-text-left-label-h1-container'>
                         <label className='header-left-logo-with-text-left-label-h1' style={ Object.assign({ fontSize: 18 }, this.props.h1Style) }>{ this.props.heading1 }</label>
